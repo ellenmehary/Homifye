33247839a0d16291de6b97ef5baab8001c8eb12b
nisha <vahoran@roehampton.ac.uk>
2022-12-05 21:19:07 +0000

    commit

--- sd2-db.sql
+++ sd2-db.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `Admin` (
-  `admin_id` int NOT NULL,
+  `admin_id` int NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(512) NOT NULL,
   `age` int,
   `gender` varchar(1) NOT NULL,
@@ -37,13 +37,35 @@ CREATE TABLE `Admin` (
   PRIMARY KEY (`admin_id`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `Home` (
+  `home_id` int NOT NULL AUTO_INCREMENT,
+  `home_name` varchar(512) NOT NULL,
+  `full_address` varchar(512) NOT NULL,
+  `admin_id` int NOT NULL,
+  FOREIGN KEY(admin_id) REFERENCES Admin(admin_id),
+  PRIMARY KEY (`home_id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE Admin_Homes(
+`admin` int NOT NULL,
+`home` int NOT NULL,
+FOREIGN KEY (admin) REFERENCES Admin(admin_id),
+FOREIGN KEY (home) REFERENCES Home(home_id))ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 --
 -- Dumping data for table `test_table`
 --
+INSERT INTO `Admin` (`admin_id`, `admin_name`, `age`, `gender`, `phone_no`, `email_id`) VALUES 
+('1', 'Ellen', '28', 'F', '1234567890', 'ellen@abc.com')
+('2', 'Zahra', '24', 'F', '0987654321', 'zahra@abc.com');
 
---INSERT INTO `` (``, ``) VALUES
---(1, 'Lisa'),
---(2, 'Kimia');
+INSERT INTO `Home` (`home_id`, `home_name`, `full_address`, `admin_id`) VALUES
+(`1`, `Zahra's Sweet Home`, `32B Star lane E34 $6G`, 2),
+(`2`, `Ellen's Sweet Home`, `32A Star Lane E34 45G`, 1);
+
+INSERT INTO `Admin_Homes` (admin, home) VALUES
+(`1`, `2`),
+(`2`, `2`);
 
 --
 -- Indexes for dumped tables
