1a9263e3ec297afeb6137c7559fc3feda7b865bd
ellenmehary <ellenmehary12@gmail.com>
2023-01-10 02:02:11 +0000

    new

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 // Create express app
 var app = express();
 
+const { User } = require("./models/user");
+
 // Add static files location
 app.use(express.static("static"));
 
@@ -15,7 +17,7 @@ app.set('views', './app/views');
 const db = require('./services/db');
 
 // Create a route for root - /
-app.get("/", function(req, res) {
+app.get("/dashboard", function(req, res) {
     res.render("dashboard");
 });
 
@@ -37,23 +39,60 @@ app.get("/homes-details", function(req, res) {
     });
 });
 
-app.get("/login", function(req, res) {
-    sql = 'select h.home_name, h.full_address, a.admin_name from Home h, Admin a where h.admin_id = a.admin_id';
-    db.query(sql).then(results => {
-    	    // Send the results rows to the all-students template
-    	    // The rows will be in a variable called data
-        res.render('login', {data: results});
+
+
+app.get('/login', function(req, res) {
+        res.render('login');
     });
+
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            match = await user.authenticate(params.password);
+            if (match) {
+                res.redirect('/' + uId);
+            }
+            else {
+                // TODO improve the user journey here
+                res.send('invalid password');
+            }
+        }
+        else {
+            res.send('invalid email');
+        }
+    } catch (err) {
+        console.error(`Error while comparing `, err.message);
+    }
 });
 
-app.get("/add-member", function(req, res) {
-    sql = 'select h.home_name, h.full_address, a.admin_name from Home h, Admin a where h.admin_id = a.admin_id';
-    db.query(sql).then(results => {
-    	    // Send the results rows to the all-students template
-    	    // The rows will be in a variable called data
-        res.render('addmember', {data: results});
+app.post('/set-password', async function (req, res) {
+        params = req.body;
+        var user = new User(params.email);
+        try {
+            uId = await user.getIdFromEmail();
+            if (uId) {
+                // If a valid, existing user is found, set the password and redirect to the users single-student page
+                await user.setUserPassword(params.password);
+                res.redirect('/' + uId);
+            }
+            else {
+                // If no existing user is found, add a new one
+                newId = await user.addUser(params.email);
+                res.send('Perhaps a page where a new user sets a programme would be good here');
+            }
+        } catch (err) {
+            console.error(`Error while adding password `, err.message);
+        }
     });
-});
+
+
+app.get('/add-member', function(req, res) {
+        res.render('addmember');
+    });
 
 // Start server on port 3000
 app.listen(3000,function(){
