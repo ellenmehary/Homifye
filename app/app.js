066efcd7143f4b995ff3b64d44bc7c3870840b6f
ellenmehary <ellenmehary12@gmail.com>
2023-01-11 15:57:22 +0000

    new

--- app/app.js
+++ app/app.js
@@ -1,24 +1,34 @@
 // Import express.js
 const express = require("express");
+
 // Create express app
 var app = express();
+
 const { User } = require("./models/user");
+
 // Add static files location
 app.use(express.static("static"));
+
 // Use the Pug templating engine
 app.set('view engine', 'pug');
 app.set('views', './app/views');
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+
 app.use(express.urlencoded({ extended: true }));
+
+
 // Create a route for root - /
 app.get("/", function(req, res) {
     res.render("homepage");
 });
+
 // Code for Dashboard/
 app.get('/dashboard', function(req, res) {
     res.render('dashboard');
 });
+
 //Home-Details Page Code//
 app.get("/homes-details", function(req, res) {
     sql = 'select h.home_name, h.full_address, a.admin_name from Home h, Admin a where h.admin_id = a.admin_id';
@@ -28,22 +38,28 @@ app.get("/homes-details", function(req, res) {
         res.render('homes-details', {data: results});
     });
 });
+
 //ADD NEW MEMBER PAGE//
 app.get('/add-member', function(req, res) {
     res.render('addmember');
 });
+
 //Login Page Code//
 app.get('/login', function(req, res) {
         res.render('login');
     });
+
 // Check submitted email and password pair
 app.post('/authenticate', async function (req, res) {
     params = req.body;
-    var user = new User(params.email);
+    console.log(params);
+    var user = new User(params.loginUser);
+    console.log(user);
     try {
         uId = await user.getIdFromEmail();
+        console.log(uId);
         if (uId) {
-            match = await user.authenticate(params.password);
+            match = await user.authenticate(params.loginPassword);
             if (match) {
                 res.redirect('/dashboard');
             }
@@ -59,10 +75,12 @@ app.post('/authenticate', async function (req, res) {
         console.error(`Error while comparing `, err.message);
     }
 });
+
 // Register Page Code//
  app.get('/register', function(req, res) {
         res.render('register');
     });
+
  // REGISTER DATA TO BACKEND//   
 app.post('/set-password', async function (req, res) {
     params = req.body;
@@ -83,10 +101,12 @@ app.post('/set-password', async function (req, res) {
             console.error(`Error while adding password `, err.message);
         }
     });
+
 // After register is successful
 app.get('/successful-register', function(req, res) {
         res.render('afterregister');
     });    
+
 // Start server on port 3000
 app.listen(3000,function(){
     console.log(`Server running at http://127.0.0.1:3000/`);
